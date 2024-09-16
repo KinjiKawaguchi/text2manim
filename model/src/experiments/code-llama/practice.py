f58ae1a3da8270839685b30d8dec8957f1f3ad63
KinjiKawaguchi <kawakin0310@icloud.com>
2024-09-16 19:13:46 +0900

    fix: Codellamaのドキュメントに従ってコードを変更

--- model/src/experiments/code-llama/practice.py
+++ model/src/experiments/code-llama/practice.py
@@ -1,4 +1,5 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer
+import transformers
 import torch
 import argparse
 
@@ -8,23 +9,29 @@ args = parser.parse_args()
 model_size = args.modelsize
 
 modelname = f"meta-llama/CodeLlama-{model_size}b-Python-hf"
-model = AutoModelForCausalLM.from_pretrained("meta-llama/CodeLlama-13b-Python-hf")
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/CodeLlama-13b-Python-hf")
+print(f"Using model: {modelname}")
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
-
-# prompt = "Please write a Python function that takes a list of integers as input and returns the sum of the list."
+tokenizer = AutoTokenizer.from_pretrained(modelname)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=modelname,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
 
 # not Instructed なのでcodeの出だしを書いてあげる?
 prompt = "import * from manim\n# Draw the graph of a quadratic function and indicate its vertex and axis."
 
-inputs = tokenizer(prompt, return_tensors="pt").to(device)
+sequences = pipeline(
+    prompt,
+    do_sample=True,
+    top_k=10,
+    temperature=0.1,
+    top_p=0.95,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    max_length=4095,
+)
 
-with torch.no_grad():
-    output = model.generate(**inputs, max_length=10000,do_sample=True,
-                            top_k=50, top_p=0.95, temperature=0.5)
-
-generated_code = tokenizer.decode(output[0], skip_special_tokens=True)
-
-print(generated_code)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
